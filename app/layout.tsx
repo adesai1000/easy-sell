d2746594d8c0fb4cc0b7b7f15b8a0aeec498ebe5
Ayush <ayushdesai1000@gmail.com>
2024-08-10 09:45:30 +0530

    Background Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header /> 
-        {children}
+        <div className="bg-gray-951 py-12">{children}</div>
         <Footer />
         </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,18 +7,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend:{
-      colors:{
-        gray:{
-          951: "f4ee5",
-          952: "#222",
-          953: "E9E4DB",
+    extend: {
+      colors: {
+        gray: {
+          951: '#f4eee5',
+          952: '#222',
+          953: '#E9E4DB',
         },
-        green:{
-          951:"#9CAE96"
-        }
-      }
-    }
+        green: {
+          951: '#9CAE96',
+        },
+      },
+    },
   },
   plugins: [],
 };
